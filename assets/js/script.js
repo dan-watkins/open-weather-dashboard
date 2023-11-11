d0917b0043129cc13d4d8a3646855ac22894930f
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-11-11 17:09:24 -0600

    got 5 day forecast up and running

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 $(function () {
   const apiKey = "5ec3f4bc98430f28dafcf83dc1eaacc4";
 
+  function saveSearch(city) {
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    if (searchHistory.indexOf(city) === -1) {
+      searchHistory.push(city);
+      localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    }
+  }
+
+  function displayWeather(response) {
+    var forecast = response.list;
+    var j = 1;
+    for (var i = 0; i < forecast.length; i++) {
+      var fDate = dayjs().add(j, "day").format("MM/DD/YYYY");
+      var fTemp = forecast[i].main.temp;
+      var fHumidity = forecast[i].main.humidity;
+      var fWind = forecast[i].wind.speed;
+      $("#day" + j).text(fDate);
+      $("#temp" + j).text("Temperature: " + fTemp + "°F");
+      $("#humidity" + j).text("Humidity: " + fHumidity + "%");
+      $("#wind" + j).text("Wind Speed: " + fWind + " MPH");
+      j++;
+    }
+  }
+
   function getWeather(city) {
     $.ajax({
       url:
@@ -12,6 +36,18 @@ $(function () {
       method: "GET",
     }).then(function (response) {
       todayWeather(response);
+    });
+
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&appid=" +
+        apiKey +
+        "&units=imperial",
+      method: "GET",
+    }).then(function (response) {
+      displayWeather(response);
     });
   }
 
